16dcb9f13036092fb5c8be5d9bd86fb6a016e967
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-02-21 09:35:21 +0300

    3-quick_sort.c

--- sort.h
+++ sort.h
@@ -27,5 +27,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap(int *array, size_t size, int idx, int wall);
+void partition(int *array, size_t size, int lo, int hi);
+
 
 #endif /* SORT_H */
